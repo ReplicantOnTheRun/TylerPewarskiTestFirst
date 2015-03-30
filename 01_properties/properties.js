57190f2cbc6f8d1642cfea5f5724bd646449f47e
TPewarski <Tjp2122@gmail.com>
2015-03-30 19:53:16 -0400

    completed 01_properties

---
+++ 01_properties/properties.js
@@ -0,0 +1,31 @@
+var setPropertiesOnObjLiteral = function(obj){
+  obj.x = 7;
+  obj.y = 8;
+  obj.onePlus = function(num){
+    return num + 1;
+  };
+};
+
+var setPropertiesOnArrayObj = function(arr){
+  arr.hello = function(){
+    return "Hello!";
+  };
+  arr.full = "stack";
+  arr[0] = 5;
+  arr.twoTimes = function(num){
+    return num * 2;
+  };
+};
+
+var setPropertiesOnFunctionObj(functionObject){
+  functionObject = function(){
+    return functionObject();
+  };
+  functionObject.year = 2015;
+  functionObject.divideByTwo = function(num){
+    return num / 2;
+  };
+  functionObject.prototype.helloWorld = function(){
+    return "Hello World";
+  };
+};
